20c637cf0ff25fefac8eb8f827a1a42c9946031b
Bryan <bryan@codebryan.com>
2019-07-28 14:42:01 -0500

    Removed build directory.

--- build/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var port = 3003;
-app.get('/', function (req, res) {
-    res.send("This is just a Git test that will be deleted later. :)");
-});
-app.listen(port, function () {
-    console.log("Listening on port " + port);
-});
